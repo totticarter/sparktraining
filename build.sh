b2cae33123751428287d220c46041111587c07c6
李勇 <liyong@MacintoshHD-6c96cfde2acf.local>
2018-04-29 08:26:01 +0800

    resolved pop confict

--- build.sh
+++ build.sh
@@ -3,5 +3,11 @@ asdf
 test for patch for multi commit
 test for patch for multi commit
 test for patch for multi commit
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 test for stash confict
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 test for stash
