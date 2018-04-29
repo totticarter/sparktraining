84d9f8c25305b56183dcf6ba637fda8d5872d720
totticarter <liyong03082218@aliyun.com>
2018-04-29 20:27:26 +0800

    aaa

--- build.sh
+++ build.sh
@@ -3,11 +3,5 @@ asdf
 test for patch for multi commit
 test for patch for multi commit
 test for patch for multi commit
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 test for stash confict
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 test for stash
